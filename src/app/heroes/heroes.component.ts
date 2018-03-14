656bd621b3ffc810be532d2512d2f1520e1e36ba
AshimaVenturePact <ashima.sukhija@venturepact.com>
2018-03-14 17:26:52 +0530

    Heroes list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,11 +8,18 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  // 1st
   // hero = 'This is content coming from Heroes Component';
-  hero: Hero = {
-    id: 1,
-    name: 'Ashima'
-  };
+
+  // 2nd
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Ashima'
+  // };
+
+  // 3rd
+  heroes = HEROES;
+
   constructor() { }
 
   ngOnInit() {
